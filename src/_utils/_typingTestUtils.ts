e9ce5c443890fb1db2e808d83561e728213c02fc
mozisan <cc.sig@icloud.com>
2019-10-26 13:00:11 +0900

    Move utility function to src/_utils/

--- src/_utils/_typingTestUtils.ts
+++ src/_utils/_typingTestUtils.ts
@@ -1,3 +1,5 @@
+import { MutableRefObject, Ref } from 'react';
+
 type ExactEqual<A, B> = readonly [A] extends readonly [B]
   ? readonly [B] extends readonly [A]
     ? true
@@ -9,3 +11,16 @@ export const expectType = <A>() => ({
     ? { readonly assert: () => void } // eslint-disable-next-line @typescript-eslint/no-explicit-any
     : never => ({ assert: () => undefined } as any),
 });
+
+export const combineRefs = <T>(...refs: readonly Ref<T>[]): Ref<T> => (
+  target: T | null,
+) => {
+  refs.forEach((ref) => {
+    if (typeof ref === 'function') {
+      ref(target);
+    } else {
+      // eslint-disable-next-line functional/immutable-data
+      ((ref as unknown) as MutableRefObject<T | null>).current = target;
+    }
+  });
+};
